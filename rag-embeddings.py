66ffbb71d4991dc2bf1e574f91d40bf44f3efdc6
Alex Volodin <alex-volodin@yandex.ru>
2025-02-09 18:02:25 +0300

    store embeddings in local file system

---
+++ rag-embeddings.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+import warnings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from loader import load_content
+from logger import get_logger
+import llm
+
+
+warnings.filterwarnings("ignore")
+
+
+if __name__ == "__main__":
+
+    # create logger
+    log = get_logger("app", __file__)
+
+    # load content
+    content = load_content("content")
+    log.info(f"{len(content)} documents loaded")
+    for document in content:
+        log.info(document.metadata.get("source"))
+
+    # split content into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200)
+    documents = text_splitter.split_documents(content)
+    log.info(f"{len(documents)} chunks created")
+
+    # load environment variables
+    load_dotenv()
+
+    # create embeddings and save to local filesystem
+    dir = "embeddings"
+    embeddings = llm.get_embeddings(os.environ)
+    db = Chroma.from_documents(persist_directory=dir,
+                               documents=documents,
+                               embedding=embeddings)
+    log.info(f"{len(documents)} chunks saved in {dir}")
--- rag-request.py
+++ rag-request.py
@@ -3,39 +3,23 @@ from dotenv import load_dotenv
 import warnings
 from langchain.chains import RetrievalQA
 from langchain_chroma import Chroma
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from loader import load_content
 from logger import get_logger
 import llm
 
-
 warnings.filterwarnings("ignore")
-
 
 if __name__ == "__main__":
 
     # create logger
     log = get_logger("app", __file__)
 
-    # load content
-    content = load_content("content")
-    log.info(f"{len(content)} documents loaded")
-    for document in content:
-        log.info(document.metadata.get("source"))
-
-    # split content into chunks
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200)
-    documents = text_splitter.split_documents(content)
-    log.info(f"{len(documents)} chunks created")
-
     # load environment variables
     load_dotenv()
 
-    # create embeddings
-    db = Chroma.from_documents(
-        documents=documents, embedding=llm.get_embeddings(os.environ))
-    log.info("put documents to vectorstore")
+    # create embeddings and save to local filesystem
+    dir = "embeddings"
+    embeddings = llm.get_embeddings(os.environ)
+    db = Chroma(persist_directory=dir, embedding_function=embeddings)
 
     # initialize LLM object
     model = llm.get_model(os.environ)
