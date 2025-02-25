767f62f23f93ec1da34c9789c4f65b5e318f82a8
Alex Volodin <alex-volodin@yandex.ru>
2025-02-25 16:40:49 +0300

    query chroma with prompt file

--- rag-query.py
+++ rag-query.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+import sys
 from dotenv import load_dotenv
 import warnings
 from langchain_chroma import Chroma
@@ -12,21 +14,26 @@ if __name__ == "__main__":
     # create logger
     log = get_logger("app", __file__)
 
-    # load environment variables
-    load_dotenv()
+    if len(sys.argv) > 1:
 
-    # import llm
-    name = os.environ.get("llm.module")
-    llm = importlib.import_module(name)
+        # load environment variables
+        load_dotenv()
 
-    # create embeddings and save to local filesystem
-    dir = "embeddings"
-    embeddings = llm.get_embeddings(os.environ)
-    db = Chroma(persist_directory=dir, embedding_function=embeddings)
+        # import llm
+        name = os.environ.get("llm.module")
+        llm = importlib.import_module(name)
 
-    # retrieve data from vectorstore
-    question = "внешние системы"
-    documents = db.similarity_search(question, k=5)
-    for document in documents:
-        print(document.page_content)
-        print("---")
+        # create embeddings and save to local filesystem
+        dir = "embeddings"
+        embeddings = llm.get_embeddings(os.environ)
+        db = Chroma(persist_directory=dir, embedding_function=embeddings)
+
+        # retrieve data from vectorstore
+        question = Path(sys.argv[1]).read_text()
+        documents = db.similarity_search(question, k=5)
+        for document in documents:
+            print(document)
+            print("---")
+
+    else:
+        log.error("No prompt file provided")
